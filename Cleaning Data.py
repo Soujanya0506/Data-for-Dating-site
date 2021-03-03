a0cd77bea2e8074343aae95b4b3e63d8493adacd
Soujanya Dash <75725533+Soujanya0506@users.noreply.github.com>
2021-03-03 23:52:45 +0530

    Data cleaning / wrangling

---
+++ Cleaning Data.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data_for_Dating_site Task 1: Is there a correlation between user's location (rural or urban) and their age?
+# ### Once we have our data, we need to determine the necessary one and in this case it is the  correlation between location and their age.
+# 
+
+# In[1]:
+
+
+# Necessary data is determined by:
+# 1. First we need to determine what data could disaprove our hypothesis (for instance, age affects dating patterns)
+# 2. Second, we find out how much data to collect. Whole population is ideal but impractical so we need to choose a sample set of data.
+# According to the Sample size calculator online, information like margin of error (in my case 5% as larger the error, the less confidence we should have), confidence level (95%), population size and Likely sample proportion (50%)
+
+
+# In[2]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# In[ ]:
+
+
+# Impor
+
